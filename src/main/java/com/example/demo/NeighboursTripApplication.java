d51fb095dceee6e2eb24ff2e72f474290e9d4d51
s.nikolov <svetlin.nikolov.91@gmail.com>
2020-05-24 22:32:53 +0300

    make spring boot application

---
+++ src/main/java/com/example/demo/NeighboursTripApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighboursTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighboursTripApplication.class, args);
+	}
+
+}
